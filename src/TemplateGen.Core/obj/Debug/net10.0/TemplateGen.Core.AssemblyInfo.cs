b65684089c407c267c970bc2a32ed03950c81583
diversifica <diversifica.ia@gmail.com>
2026-01-01 20:20:23 +0100

    feat: add document metadata support (closes #31)
    - Set document title from client name
    - Set creator as TemplateGen
    - Set description from profile metadata
    - Set created/modified timestamps
    - Set version from profile version

--- src/TemplateGen.Core/obj/Debug/net10.0/TemplateGen.Core.AssemblyInfo.cs
+++ src/TemplateGen.Core/obj/Debug/net10.0/TemplateGen.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TemplateGen.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77c219011427dacf5cee1182497d494ebeb4c592")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48eb8fb36a59679b8098ee73dc4a66dd57fae86b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TemplateGen.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TemplateGen.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
